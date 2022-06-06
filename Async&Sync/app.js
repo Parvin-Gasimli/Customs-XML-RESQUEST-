1eea58ac9f780574512a4e4d3880217260c71b80
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2022-06-06 20:56:17 +0400

    test

--- Async&Sync/app.js
+++ Async&Sync/app.js
@@ -1,18 +1,24 @@
-async function myFunc() {
-  const promise = new Promise((reslove, rejected) => {
-    setTimeout(() => reslove("hello"), 1000);
-  });
-  const error = true;
-  if (!error) {
-    return res;
-  } else {
-    await Promise.reject(new Error("something went wrong"));
-  }
-  const res = await promise;
-  return res;
+// async function myFunc() {
+//   const promise = new Promise((reslove, rejected) => {
+//     setTimeout(() => reslove("hello"), 1000);
+//   });
+//   const error = true;
+//   if (!error) {
+//     return res;
+//   } else {
+//     await Promise.reject(new Error("something went wrong"));
+//   }
+//   const res = await promise;
+//   return res;
+// }
+
+// myFunc()
+// .then((res) => console.log(res))
+// .catch(err=>console.log(err))
+
+async function getUsers(){
+  const response=await fetch("https://jsonplaceholder.typicode.com/posts")
+  const data=await response.json()
+  return data
 }
-
-myFunc()
-.then((res) => console.log(res))
-.catch(err=>console.log(err))
-
+getUsers().then(user=>console.log(user))
