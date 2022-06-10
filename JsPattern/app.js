7a371db54a53116bc6a068d46348f17b1d28b90c
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2022-06-10 13:30:33 +0400

    Module Pateern

--- JsPattern/app.js
+++ JsPattern/app.js
@@ -41,5 +41,175 @@ const ItemCtrl = (function () {
     get: get,
   };
 })();
+ItemCtrl.add({ id: 1, name: "Parvin" });
+console.log(ItemCtrl.get(1));
 
-ItemCtrl.add({ id: 1, name: "Parvun" });
+//Single Pattern
+const singgelton = (function () {
+  let instance;
+  function createInstance() {
+    const object = new Object({ id: 1 });
+    return object;
+  }
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    },
+  };
+})();
+
+const instanceA = singgelton.getInstance();
+console.log(instanceA);
+
+// //Factory Pattern
+// function memberFactory(){
+//   this.createmember=function(name,type){
+//     let member;
+//     if(type==='simple'){
+//       member=new SimpleMemebership(name)
+//     }
+//     else if(type==='standart'){
+//       member=new SatandartMembership(name)
+
+//     }
+//     else if(type==='super'){
+//       member=new SuperMembership(name)
+//     }
+
+//     member.type=type;
+//     member.define=function(){
+//       console.log(`${this.name} (${this.type}):${this.cost}`);
+//     }
+//     return member
+
+//   }
+// }
+// const SimpleMemebership=function(name){
+//   this.name=name;
+//   this.cost='$5'
+
+// }
+// const SatandartMembership=function(name){
+//   this.name=name;
+//   this.cost='$12'
+
+// }
+// const SuperMembership=function(name){
+//   this.name=name;
+//   this.cost ='$23'
+
+// }
+
+// const members=[];
+// const factory=new memberFactory()
+// members.push(factory.createmember("John doe","simple"))
+// members.push(factory.createmember("John doe1","simple2"))
+// members.push(factory.createmember("John doe3","simple3"))
+// members.push(factory.createmember("John doe4","simple4"))
+// console.log(members);
+
+// //Observe Pattern
+// function EventObserve() {
+//   this.observe = [];
+// }
+// EventObserve.prototype = {
+//   subscribe: function (fn) {
+//     this.observe.push(fn);
+//     console.log(`you are now subscribed to ${fn} name`);
+//   },
+//   unsubscribe: function (fn) {
+//     this.observe = this.observe.filter(function (item) {
+//       if (item !== fun) {
+//         return item;
+//       }
+//     });
+//     console.log(`you are un unscribed fro,${fn.name}`);
+//   },
+//   fire: function(){
+//     this.observe.forEach( function(item){
+//       item.call()
+//     })
+//   }
+
+// };
+
+// const Click =EventObserve()
+// document.querySelector(".sub-ms").addEventListener('click',function(){
+//   Click.subscribe(getCurMilliSecond)
+// })
+// document.querySelector(".unsub-ms").addEventListener('click',function(){
+//   Click.unsubscribe(getCurMilliSecond)
+// })
+// document.querySelector(".unsub-ms").addEventListener('click',function(){
+//   Click.fire()
+// })
+// const getCurMilliSecond=function(){
+//   console.log(`current Milli Second ${new Date().toMilliseconds()}`);
+// }
+
+//Meadiator Pattern
+// const user = function (name) {
+//   this.name = name;
+//   this.Chatroom = null;
+// };
+// user.prototype = {
+//   send: function (message, to) {
+//     this.Chatroom.send(message, this, to);
+//   },
+//   receive: function (message, from) {
+//     console.log(`${from.name} to ${this.name}:${message}`);
+//   },
+// };
+// const Chatroom = function () {
+//   let users = {};
+//   return {
+//     register: function (user) {
+//       users[user.name] = user;
+//       user.Chatroom = this;
+//     },
+//     send: function (message, from, to) {
+//       if (to) {
+//         to.receive(message,from)
+//       } else {
+//         for(key in users){
+//           if(user[key]!==from){
+//             user[key].receive(message,from)
+//           }
+//         }
+//       }
+//     },
+//   };
+// };
+// const brad=new user('brad')
+// const jeff=new user('Jeff')
+// const Sara=new user('Sara')
+// const chatroom=new Chatroom()
+// chatroom.register(brad)
+// chatroom.register(jeff)
+// chatroom.register(Sara)
+// brad.send('hello Jeff',jeff)
+// brad.send('hello Sara how are you?',jeff)
+
+//small Pattern
+// const PageSatet=function(){
+//   let currentState=new homeState(this)
+//   this.init =function(){
+//     this.change(new homeState)
+//   }
+//   this.change=function(){
+//     currentState=state;
+//   }
+
+// }
+
+// //Home state
+// const homeState=function(page){
+//   document.querySelector('#heading').textContent=null
+//   document.querySelector('#content').innerHTML=`
+  
+  
+//   `
+//}
--- destructing/app.js
+++ destructing/app.js
@@ -1,76 +1,129 @@
-let a,b;
-// [a,b]=[10,20]
-// [a,b,c,...rest]=[10,20,30,40,50,60]
-// ({a,b}={a:100,b:200,c:300,d:400})
-// ({c,d, ...rest}={c:1,d:2,e:4,f:5})
-// console.log(c);
-////array destructing
-// const people=["John","Mike","Bane"]
-// const[person1,person2,person3]=people;
-// console.log(person1,person2,person3);
+// // let a,b;
+// // [a,b]=[10,20]
+// // [a,b,c,...rest]=[10,20,30,40,50,60]
+// // ({a,b}={a:100,b:200,c:300,d:400})
+// // ({c,d, ...rest}={c:1,d:2,e:4,f:5})
+// // console.log(c);
+// // //array destructing
+// // const people=["John","Mike","Bane"]
+// // const[person1,person2,person3]=people;
+// // console.log(person1,person2,person3);
+
+
+
+// // ({...data}={name:"ulvi",surname:"Nuriyev",age:23,score:99,Professin:'develoepr',Birthday:'....05.05.1998'})
+
+// // console.log(data);
+
+
+// // const person={
+// //     name:"ulvi",
+// //     surname:"Nuriyev"
+// // }
+// // console.log(person.name);
+
+// // [c,e,...f]=[1,2,3,4,5,6,7,8,9,10]
+// // console.log(c);
+// // console.log(e);
+// // console.log(f);
+// // [a,b,c,d]=[10 ,[11,23,44],12,13]
+// // a=34
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
+// // console.log(d);
+
+// // const name="eli",
+// // surname="babayev",
+// // age=30,
+// // score=45
+
+// // console.log(`name ${name} surname:${surname},age:${age} score${score}`);
+// // console.log(age);
+
+
+
 
 // function getPeople(){
 //     return ['johns',"mike","Don"]
 // }
 
-// let person,person4,person5;
-// [person,person4,person5]=getPeople()
-// console.log(person,person4,person5);
+// // let person,person4,person5;
+// // [person,person4,person5]=getPeople()
+// // console.log(person,person4,person5);
 
 
-//OBject Destructing
+// //OBject Destructing
 
-// const person={
-//     name:"john Doe",
-//     age:34,
-//     city:"Miami",
-//     gender:"male"
-// }
-// // //ES 5
-// // const name=person.name,
-// //                     age=person.age,
-// //                     city=person.city,
-// //                     gender=person.gender;
+// // const person={
+// //     name:"john Doe",
+// //     age:34,
+// //     city:"Miami",
+// //     gender:"male"
+// // }
+// //ES 5
+// const name=person.name,
+//                     age=person.age,
+//                     city=person.city,
+//                     gender=person.gender;
 
 // //ES6
 //  const  {name,age,city,gender}=person
 //  console.log(name,age,city,gender);
 
 
-//Map ES 6
+ 
+
+// // Map ES 6
 
 
-const map1=new Map()
-const key1="some String",key2={},key3= function(){};
+// // const map1=new Map()
+// // const key1="some String",key2={},key3= function(){};
 
 
-map1.set(key1,"value of key1")
-map1.set(key2,"value of key2")
-map1.set(key3,"value of key3")
-console.log(map1.get(key1),map1.get(key2),map1.get(key3));
+// // map1.set(key1,"value of key1")
+// // map1.set(key2,"value of key2")
+// // map1.set(key3,"value of key3")
+// // console.log(map1.get(key1),map1.get(key2),map1.get(key3));
 
 
-console.log(map1.size);
+// // console.log(map1.size);
 
-//Interating Maps
-//lop using for to get key and values
-// for (let [key,value] of map1) {
-//     console.log(`${key}=${value}`);
-// }
-// for (let key of map1.keys()) {
-//     console.log(`${key}`);
-// }
-// for (let key of map1.values()) {
-//     console.log(`${key}`);
-// }
+// // Interating Maps
+// // lop using for to get key and values
+// // for (let [key,value] of map1) {
+// //     console.log(`${key}=${value}`);
+// // }
+// // for (let key of map1.keys()) {
+// //     console.log(`${key}`);
+// // }
+// // for (let key of map1.values()) {
+// //     console.log(`${key}`);
+// // }
 
 
-//SETS -Store uniq value of any type
-const set1= new Set()
-set1.add(100)
-set1.add("a string")
-set1.add({name:"ali"})
-set1.add(true)
-set1.add(100)
-set1.add(200)
-console.log(set1);
+// // SETS -Store uniq value of any type
+// // const set1= new Set()
+// // set1.add(100)
+// // set1.add("a string")
+// // set1.add({name:"ali"})
+// // set1.add(true)
+// // set1.add(100)
+// // set1.add(200)
+// // console.log(set1);
+ let start=new Date()
+ start.getMilliseconds
+
+let a=[4,5,6,]
+console.log(a[0]);
+console.log(a.length);
+let salam="zeyno ve sofer"
+setTimeout(() => {
+    console.log(salam);
+    
+}, 3000);  
+
+
+let end = new Date()
+end.getMilliseconds
+console.log(start.getTime()-end.getTime());
